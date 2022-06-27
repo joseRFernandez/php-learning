1c750faa0caeb3dc105debc75bb0fdce3c95e48d
Jose_Fernandez <jfernand84@gmail.com>
2022-06-27 17:07:15 -0400

    practice project --> working on header links

--- tuts/index.php
+++ tuts/index.php
@@ -1,29 +1,12 @@
 <?php
 
-$name= 'James';
-$name2='jose'
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My First Php Embedded Doc</title>
-</head>
-<body>
-
 <?php
 
-  $array_1 = ['Jose', 'Stefanie', 'James', 'Sadie'];
-  ?>
+include('templates/header.php');
 
-  <?php
-  foreach($array_1 as $name) { ?>
-    <h1><?php echo $name ?></h1>
-  <?php } ?>
-
-
+?>
 </body>
 </html>
